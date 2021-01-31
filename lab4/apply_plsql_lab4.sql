c1d4e31b9ac3ef709267f881c5efe6b18b5cc083
edwardsja <edw15013@byui.edu>
2021-01-30 23:56:29 -0700

    lab4 complete

--- lab4/apply_plsql_lab4.sql
+++ lab4/apply_plsql_lab4.sql
@@ -12,6 +12,52 @@
 SPOOL apply_plsql_lab4.txt
 
 -- Enter your solution here.
+SET SERVEROUTPUT ON
 
+CREATE OR REPLACE 
+TYPE lyric IS OBJECT
+( day_name VARCHAR2(8)
+, gift_name VARCHAR2(24));
+/
+
+
+DECLARE
+
+TYPE gifts IS TABLE OF lyric;
+TYPE days IS TABLE OF VARCHAR2(8);
+
+lv_days DAYS := days('first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth');
+
+lv_gifts GIFTS := gifts( lyric('and a', 'Partridge in a pear tree')
+                       , lyric('Two', 'Turtle doves')
+                       , lyric('Three', 'French hens')
+                       , lyric('Four', 'Calling Birds')
+                       , lyric('Five', 'Golden rings')
+                       , lyric('Six', 'Geese a laying')
+                       , lyric('Seven', 'Swans a swimming')
+                       , lyric('Eight', 'Maids a Milking')
+                       , lyric('Nine', 'Ladies dancing')
+                       , lyric('Ten', 'Lords a leaping')
+                       , lyric('Eleven', 'Pipers piping')
+                       , lyric('Twelve', 'Drummers drumming'));
+BEGIN 
+    FOR i IN 1..lv_gifts.COUNT LOOP
+        dbms_output.put_line('On the '||lv_days(i)||' day of Christmas');
+        dbms_output.put_line(CHR(13));
+        dbms_output.put_line('my true love sent to me:');
+        IF i = 1 THEN
+            FOR j IN REVERSE 1..i LOOP
+            dbms_output.put_line(CHR(13));
+            dbms_output.put_line('A '||lv_gifts(j).gift_name);
+            END LOOP;
+        ELSE
+            FOR j IN REVERSE 1..i LOOP
+            dbms_output.put_line(CHR(13));
+            dbms_output.put_line(lv_gifts(j).day_name||' '||lv_gifts(j).gift_name);
+            END LOOP;
+        END IF;
+    END LOOP;
+END;
+/
 -- Close log file.
 SPOOL OFF
